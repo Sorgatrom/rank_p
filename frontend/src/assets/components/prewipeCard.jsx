23509b1a59f29d3bab5f80cfd4cf773ca40ff9ab
Sorgatrom <man.andalc@icloud.com>
2026-05-11 11:53:35 +0200

    feat: Se Añade la reacción al boton like, ahora usamos un hook y lo dejamos listo para la lógica con el serv.

--- frontend/src/assets/components/prewipeCard.jsx
+++ frontend/src/assets/components/prewipeCard.jsx
@@ -3,26 +3,33 @@
     import { generateFakeName } from "../utils/generador_nombres"
 
     function PreWipeCard({number, content}) {
-    const [count, setCount] = useState(0)
 
-    //Con esta solucion solo se genera una vez el nombre falso, y se mantiene el mismo aunque el componente se vuelva a renderizar por el cambio de estado del contador.
-    const [name, setName] = useState(() => generateFakeName());
+        
+        const [like, setlike] = useState(false);
 
-    return (
-        <>
-            <div className="prewipe-card">
-                <div className="prewipe-card-number">
-                    <p>{number}</p>
+        function toggleLike() {
+            //Aquí se podría agregar lógica adicional para manejar el estado del "like", como enviar una solicitud a un servidor o actualizar un contador de "likes". Por ahora, simplemente alternamos el estado entre "like" y "unlike".
+            setlike(!like);
+        }
+
+        //Con esta solucion solo se genera una vez el nombre falso, y se mantiene el mismo aunque el componente se vuelva a renderizar por el cambio de estado del contador.
+        const [name, setName] = useState(() => generateFakeName());
+
+        return (
+            <>
+                <div className="prewipe-card">
+                    <div className="prewipe-card-number">
+                        <p>{number}</p>
+                    </div>
+                    <div className="prewipe-card-fakenameandcontent">
+                        <h2>{name}</h2>
+                        <p>{content}</p>
+                    </div>
+                    <div className={`prewipe-card${like ? '-liked' : '-like'}`}>
+                        <button onClick={toggleLike}>{like ? 'Like' : 'Unlike'}</button>
+                    </div>
                 </div>
-                <div className="prewipe-card-fakenameandcontent">
-                    <h2>{name}</h2>
-                    <p>{content}</p>
-                </div>
-                <div className="prewipe-card-like">
-                    <button onClick={() => setCount(count + 1)}>Like</button>
-                </div>
-            </div>
-        </>
-    )};
+            </>
+        )};
 
     export default PreWipeCard;
