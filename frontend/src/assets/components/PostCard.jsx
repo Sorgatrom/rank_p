aacfe81e33be97d9d3f323c01a7f883fadea523d
Sorgatrom <man.andalc@icloud.com>
2026-05-27 14:46:28 +0200

    feat: Ahora hay una barra de color que notifica si has gastado tus límite de post, se cuentan y se notifica abajo.

--- frontend/src/assets/components/PostCard.jsx
+++ frontend/src/assets/components/PostCard.jsx
@@ -1,9 +1,39 @@
 import './postcard.css'
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CategoriaContext } from '../components/minis/CategoriasProvisor';
 
 function PostCard({}) {
     const { categoriaElegida } = useContext(CategoriaContext);
+
+    //Creamos el estado para guardar el texto y definir el límite.
+    const [texto, setTexto] = useState("");
+    const limiteCaracteres = 262; //Poesia "Es verdad" de Lorca; demostró que se puede contar mucho con poco. Aprendamos.
+    //Para controlar la barra de colores
+    const [barra, setBarra] = useState("");
+    //La función se dispara cada vez que el usuario pulsa una tecla
+    const manejarCambio = (evento) => {
+        const nuevoTexto = evento.target.value;
+        
+        // Verificamos por seguridad (aunque el maxLength de HTML ya nos ayuda)
+        if (nuevoTexto.length <= limiteCaracteres) {
+            setTexto(nuevoTexto);
+        }
+
+        //Para manejar los estados de la barra
+        if (nuevoTexto.length <= 52.5 ){
+            setBarra("postcard-counter")
+        } else if (nuevoTexto.length > 52.6 && nuevoTexto.length <= 105){
+            setBarra("postcard-counter-green")
+        } else if (nuevoTexto.length > 106 && nuevoTexto.length <= 158){
+            setBarra("postcard-counter-yellow")
+        } else if (nuevoTexto.length > 159 && nuevoTexto.length <= 212){
+            setBarra("postcard-counter-orange")
+        } else if (nuevoTexto.length > 213 && nuevoTexto.length <= 262){
+            setBarra("postcard-counter-red")
+        };
+    };
+
+
 
     return (
         <div className="postcard-main-container">
@@ -14,10 +44,15 @@ function PostCard({}) {
                         <textarea 
                             rows="4" 
                             placeholder={`Escribe algo sobre ${categoriaElegida}`} 
-                            maxLength="280" 
+                            maxLength={limiteCaracteres} 
+                            value={texto}
+                            onChange={manejarCambio}
                             autoFocus
                         />
-                        <div className="postcard-counter"></div>
+                        <p>{texto.length} / {limiteCaracteres} Tip: Longitud del poema "Es Verdad" de F.G. Lorca</p>
+                        <div className={barra}>
+
+                        </div>
                     </div>
                     <button className="postcard-button" type="submit">Publicar</button>
                 </form>
